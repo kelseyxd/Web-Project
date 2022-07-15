a7e178d935acc51ea58d9d7b2c311982494e78b9
Limchienhui <midnightstxrs@gmail.com>
2022-07-15 17:58:55 +0800

    Item Page Updates

--- frontend/src/components/pages/Item.jsx
+++ frontend/src/components/pages/Item.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
 import Desserts from "../../data.json";
 import { useLocation } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function Item() {
+  const [quantity, setQuantity] = useState(0);
+
   const location = useLocation();
   const { name, image, price } = location.state;
   return (
-    <div>
-      <img src={image} />
-      <h1>{name}</h1>
-      <p>{price}</p>
-    </div>
+    <Container style={{ margin: "2%" }}>
+      <Row>
+        <Col>
+          <img src={image} width="500" height="400" />
+        </Col>
+        <Col>
+          <h1>{name}</h1>
+          <p>{price}</p>
+          <Row>
+            <Button
+              variant="light"
+              style={{ marginRight: "1%" }}
+              onClick={() => setQuantity(quantity - 1)}
+            >
+              -
+            </Button>
+            <Button variant="light">{quantity}</Button>
+            <Button
+              variant="light"
+              style={{ marginLeft: "1%" }}
+              onClick={() => setQuantity(quantity + 1)}
+            >
+              +
+            </Button>
+          </Row>
+          <Button style={{ marginTop: "1%", width: "11%" }}>Add to Cart</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
