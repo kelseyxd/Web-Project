dc9712256bd617f173debbf7f5ac91dee59f504a
Limchienhui <midnightstxrs@gmail.com>
2022-07-16 03:50:31 +0800

    added text if cart empty; need to fix navrbar css error!

--- frontend/src/components/pages/Cart.js
+++ frontend/src/components/pages/Cart.js
@@ -1,25 +1,45 @@
 import React from "react";
 import "../../App.css";
 import CartItem from "../CartItem";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   const cartFromLocalStorage = JSON.parse(localStorage.getItem("myCart"));
-  return (
-    <div className="cart">
-      <h1 className="cartTitle">Your Cart</h1>
-      <div className="cartList">
-        {cartFromLocalStorage.map((cartItem, key) => {
-          return (
-            <CartItem
-              key={key}
-              image={cartItem.image}
-              name={cartItem.name}
-              price={cartItem.price}
-              quantity={cartItem.quantity}
-            />
-          );
-        })}
+
+  if (cartFromLocalStorage) {
+    return (
+      <div className="cart">
+        <h1 className="cartTitle">Your Cart</h1>
+        <div className="cartList">
+          {cartFromLocalStorage.map((cartItem, key) => {
+            return (
+              <CartItem
+                key={key}
+                image={cartItem.image}
+                name={cartItem.name}
+                price={cartItem.price}
+                quantity={cartItem.quantity}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="emptyCart">
+        <h1 className="emptyCartTitle">No items in Cart!</h1>
+        <Link
+          to={{
+            pathname: `/menu`,
+          }}
+        >
+          <Button variant="link">
+            <h4>Browse our menu to add some desserts</h4>
+          </Button>
+        </Link>
+      </div>
+    );
+  }
 }
